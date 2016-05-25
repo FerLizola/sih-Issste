6df88d6560f0dbaca569b6d436efef6b2e4d2cab
abril <abril@abril-Inspiron-3442>
2016-05-25 01:01:54 -0600

    QUE MODIFICACIONES HICISTE? AQUI PONES LO QUE QUIERAS PUEJ

--- SIH_ISSSTE/src/Incapacidad.java
+++ SIH_ISSSTE/src/Incapacidad.java
@@ -18,7 +18,8 @@ import javax.swing.JOptionPane;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+CUANDO QUIERAS HACER UNA ACTUALIZACION DESPUES DE HABER HECHO MODIFICACIONES COMO AHORITA CON UN 
+SIMPLE COMENTARIO HACES LO SIGUIENTE*/
 
 /**
  *
